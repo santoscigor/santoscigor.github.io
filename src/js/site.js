b200a8f8ca23d4ffb11fc3266b1cd905000c486e
Igor Costa dos Santos <igor.santos@cubos.io>
2021-06-20 18:58:13 -0300

    feat: header structure

--- src/js/site.js
+++ src/js/site.js
@@ -1,4 +1,41 @@
-document.addEventListener('DOMContentLoaded', () => {
-    let elems = document.querySelectorAll('.sidenav');
-    let instances = M.Sidenav.init(elems);
-  });
+let didScroll = false;
+let lastScrollTop = 0;
+const delta = 5;
+const navbarHeight = document.querySelector('header').offsetHeight;
+
+
+window.onscroll = function() {
+    didScroll = true;
+};
+
+window.addEventListener('scroll', function() {
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+      hasScrolled();
+      didScroll = false;
+    }
+}, 250);
+
+const hasScrolled = () => {
+    const scrollTop = window.scrollY;
+
+    if(Math.abs(lastScrollTop - scrollTop) <= delta)
+        return;
+
+    if (scrollTop > lastScrollTop && scrollTop > navbarHeight){
+        // Scroll Down
+        document.querySelector('header').classList.remove('nav-down');
+        document.querySelector('header').classList.add('nav-up');
+    } else {
+        // Scroll Up
+        if(scrollTop + window.innerHeight < document.body.clientHeight) {
+            document.querySelector('header').classList.remove('nav-up');
+            document.querySelector('header').classList.add('nav-down');
+        }
+    }
+        
+    lastScrollTop = scrollTop;
+}
