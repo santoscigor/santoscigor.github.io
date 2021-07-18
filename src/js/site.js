687e39e2ff09417660c2b889f691f008540aa0a0
Igor Costa dos Santos <santoscigor@gmail.com>
2021-07-18 17:53:59 -0300

    feat: dark mode

--- src/js/site.js
+++ src/js/site.js
@@ -2,7 +2,28 @@ let didScroll = false;
 let lastScrollTop = 0;
 const delta = 5;
 const navbarHeight = document.querySelector('header').offsetHeight;
+const currentTheme = localStorage.getItem("theme");
+const nightmodeButton = document.querySelector(".switch");
 
+if (currentTheme == "dark") {
+    document.body.classList.add("dark-theme");
+    document.getElementById("darkmodecheckbox").checked = true;
+}
+
+nightmodeButton.addEventListener("click", function(event) {
+    document.body.classList.toggle("dark-theme");
+  
+    let theme = "light";
+    if (document.body.classList.contains("dark-theme")) {
+      theme = "dark";
+      document.getElementById("darkmodecheckbox").checked = true;
+    } else {
+        document.getElementById("darkmodecheckbox").checked = false;
+    }
+
+    localStorage.setItem("theme", theme);
+    event.preventDefault();
+});
 
 window.onscroll = function() {
     didScroll = true;
@@ -26,11 +47,9 @@ const hasScrolled = () => {
         return;
 
     if (scrollTop > lastScrollTop && scrollTop > navbarHeight){
-        // Scroll Down
         document.querySelector('header').classList.remove('nav-down');
         document.querySelector('header').classList.add('nav-up');
     } else {
-        // Scroll Up
         if(scrollTop + window.innerHeight < document.body.clientHeight) {
             document.querySelector('header').classList.remove('nav-up');
             document.querySelector('header').classList.add('nav-down');
