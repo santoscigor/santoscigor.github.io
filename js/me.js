d19a3bb5de1e6449ecba87af6f773291bc3cc94b
Igor Costa dos Santos <santoscigor@gmail.com>
2021-08-22 12:46:58 -0300

    refactor: linting

--- js/me.js
+++ js/me.js
@@ -1,23 +1,22 @@
-const shortButton = document.getElementById("short-button");
-const longButton = document.getElementById("long-button");
-const longBio = document.getElementById("bio-long");
-const shortBio = document.getElementById("bio-short");
+const shortButton = document.getElementById('short-button')
+const longButton = document.getElementById('long-button')
+const longBio = document.getElementById('bio-long')
+const shortBio = document.getElementById('bio-short')
 
+shortButton.addEventListener('click', () => {
+  longBio.style.display = 'none'
+  shortBio.style.display = 'block'
+  longButton.style.color = 'var(--text-color)'
+  longButton.style.border = 'var(--text-color) solid 0.1rem'
+  shortButton.style.color = 'tomato'
+  shortButton.style.border = 'tomato solid 0.1rem'
+})
 
-shortButton.addEventListener("click", () => {
-    longBio.style.display = "none";
-    shortBio.style.display = "block";
-    longButton.style.color = "var(--text-color)";
-    longButton.style.border = "var(--text-color) solid 0.1rem";
-    shortButton.style.color = "tomato";
-    shortButton.style.border = "tomato solid 0.1rem";
-});
-
-longButton.addEventListener("click", () => {
-    shortBio.style.display = "none";
-    longBio.style.display = "block";
-    shortButton.style.color = "var(--text-color)";
-    shortButton.style.border = "var(--text-color) solid 0.1rem";
-    longButton.style.color = "tomato";
-    longButton.style.border = "tomato solid 0.1rem";
+longButton.addEventListener('click', () => {
+  shortBio.style.display = 'none'
+  longBio.style.display = 'block'
+  shortButton.style.color = 'var(--text-color)'
+  shortButton.style.border = 'var(--text-color) solid 0.1rem'
+  longButton.style.color = 'tomato'
+  longButton.style.border = 'tomato solid 0.1rem'
 })
